95419a9665e2aa0983a508f1273c165463bafd1e
Isabella Ramare <isabella.ramare@hotmail.com>
2019-05-14 14:36:11 +0200

    försökte fixa edit

--- finder-ui/Controllers/ServiceController.cs
+++ finder-ui/Controllers/ServiceController.cs
@@ -63,15 +63,16 @@ namespace finder_ui.Controllers
                 client.CreateService(
                     createServiceObject.Type,
                     createServiceObject.CreatorId,
-                       createServiceObject.ServiceStatusId,
-                       createServiceObject.Picture,
-                       createServiceObject.Title,
-                       createServiceObject.Description,
-                       createServiceObject.Price,
-                       createServiceObject.StartDate,
-                       createServiceObject.EndDate,
-                       createServiceObject.TimeNeeded,
-                       createServiceObject.SubCategoryId);
+                    createServiceObject.ServiceStatusId,
+                    createServiceObject.Picture,
+                    createServiceObject.Title,
+                    createServiceObject.Description,
+                    createServiceObject.Price,
+                    createServiceObject.StartDate,
+                    createServiceObject.EndDate,
+                    createServiceObject.TimeNeeded,
+                    createServiceObject.SubCategoryId);
+
                 return RedirectToAction("Index");
             }
             catch
@@ -83,17 +84,67 @@ namespace finder_ui.Controllers
         // GET: Service/Edit/5
         public ActionResult Edit(int id)
         {
-            
-            return View();
+            /*var service = client.GetServiceById(id);
+    
+            EditServiceObject editService = new EditServiceObject(
+                service.Id,
+                service.ServiceType.Id,
+                service.ServiceStatus.Id,
+                service.Picture,
+                service.Title,
+                service.Description,
+                service.Price,
+                service.StartDate,
+                service.EndDate,
+                service.TimeNeeded,
+                service.SubCategory.Id);*/
+
+            return View(client.GetServiceById(id));
         }
 
         // POST: Service/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(
+            int id, 
+            int type,
+            int serviceStatusId,
+            string picture,
+            string title,
+            string description,
+            double price,
+            DateTime? startDate,
+            DateTime? endDate,
+            bool timeNeeded,
+            int subCategoryId)
         {
             try
             {
-                // TODO: Add update logic here
+                /*EditServiceObject editServiceObject = new EditServiceObject(
+                    id,
+                    type,
+                    serviceStatusId,
+                    picture,
+                    title,
+                    description,
+                    price,
+                    startDate,
+                    endDate,
+                    timeNeeded,
+                    subCategoryId);*/
+
+                bool editOk = 
+                client.EditService(
+                    id,
+                    type,
+                    serviceStatusId,
+                    picture,
+                    title,
+                    description,
+                    price,
+                    startDate,
+                    endDate,
+                    timeNeeded,
+                    subCategoryId);
 
                 return RedirectToAction("Index");
             }
---
+++ finder-ui/Models/EditServiceObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using finder_ui.Group3ServiceReference;
+
+namespace finder_ui.Models
+{
+    public class EditServiceObject
+    {
+
+        public EditServiceObject(int id, int type, int serviceStatusId, string picture, string title, string description, double price, DateTime? startDate, DateTime? endDate, bool timeNeeded, int subCategoryId)
+        {
+            Id = id;
+            Type = Type;
+            ServiceStatusId = serviceStatusId;
+            Picture = picture;
+            Title = title;
+            Description = description;
+            Price = price;
+            StartDate = startDate;
+            EndDate = endDate;
+            TimeNeeded = timeNeeded;
+            SubCategoryId = subCategoryId;
+        }
+
+        public int Id { get; set; }
+        public int Type { get; set; }
+        public int ServiceStatusId { get; set; }
+        public string Picture { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool TimeNeeded { get; set; }
+        public int SubCategoryId { get; set; }
+    }
+}
