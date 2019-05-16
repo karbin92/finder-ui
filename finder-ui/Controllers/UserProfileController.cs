f65e2f2fd26ea2a46ee052ea5d9708bf1e2b576a
Christoffer Andersson <31024266+junkoffer@users.noreply.github.com>
2019-05-16 11:16:46 +0200

    Added linus stuff

--- finder-ui/Controllers/UserProfileController.cs
+++ finder-ui/Controllers/UserProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using finder_ui.Models;
 
 namespace finder_ui.Controllers
 {
@@ -11,10 +12,41 @@ namespace finder_ui.Controllers
         // GET: UserProfile
         public ActionResult Index()
         {
-            UserProfileServiceReference.UserProfileServiceClient client = new UserProfileServiceReference.UserProfileServiceClient();
-             
+            using (var client = new UserProfileServiceReference.UserProfileServiceClient())
+            {
+                var User = client.GetUserByUserName("Clark");
+
+            }
+
 
             return View();
+        }
+        public ActionResult EditProfile(EditProfileViewModel vm)
+        {
+            var UpdateProfile = new EditProfileViewModel();
+            return View(UpdateProfile);
+        }
+        public ActionResult UpdateAccountInformation(EditProfileViewModel vm)
+        {
+            using (var client = new UserProfileServiceReference.UserProfileServiceClient())
+            {
+                var updateUser = new UserProfileServiceReference.User()
+                {
+                    Address = vm.userAddress,
+                    City = vm.userCity,
+                    Email = vm.userEmail,
+                    Phonenumber = vm.userPhoneNumber,
+                    Picture = vm.userProfilePicture,
+                    ZipCode = vm.userZipCode,
+                    Id = vm.id,
+
+                };
+
+                var user = client.UpdateUser(updateUser);
+            }
+
+            return View("Index");
+
         }
     }
 }
