ec4b76087d35e02f3e658a5a6431799f8dd89ee0
eddo555 <43670839+eddo555@users.noreply.github.com>
2019-05-21 14:18:10 +0200

    messagecontroller - fixat sessionId till int.

--- finder-ui/Controllers/MessageController.cs
+++ finder-ui/Controllers/MessageController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static finder_ui.SessionHandler.CustomAuthorization;
 
 namespace finder_ui.Controllers
 {
+    //[Authorize]
+    [CustomAuthorization]
     public class MessageController : Controller
-    {//Will
-        // GET: Message
+    {
+        MessageServiceReference.Service1Client messageClient = new MessageServiceReference.Service1Client();
+        Group3ServiceReference.Service1Client adClient = new Group3ServiceReference.Service1Client();
         public ActionResult Index()
         {
+
+           //konvertera sessionid från objekt till int
+            int sessId = Convert.ToInt32(Session["UserId"]);
+            IEnumerable<MessageServiceReference.Messageinfo> messageList = messageClient.GetMessages().ToList();
+            ViewBag.Lista = messageList;
+
+            //viewbag testa sessionId
+            ViewBag.sessId = Session["UserID"];
+            ViewBag.userMedd = messageClient.GetUserMessage(sessId);
+
+            ViewBag.annonsClient = adClient.GetAllServiceData();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MessageServiceReference.AddMessage newMessage)
+        {
+            MessageServiceReference.Service1Client messageClient = new MessageServiceReference.Service1Client();
+            messageClient.AddMessage(newMessage);
+            IEnumerable<MessageServiceReference.Messageinfo> messagelist = messageClient.GetMessages().ToList();
+            //IEnumerable<MeddelandeServiceReference.Messageinfo> messagelist = messageClient.GetMessages().ToList().Where(m => m.SendingUserId == 1);
+            ViewBag.Lista = messagelist;
             return View();
         }
     }
