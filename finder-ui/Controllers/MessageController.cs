f6576cac5c4cb534dea4790e49f4d6003fbae89f
William Brushwood <wibr0002@student.hv.se>
2019-05-22 10:41:29 +0200

    test

--- finder-ui/Controllers/MessageController.cs
+++ finder-ui/Controllers/MessageController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static finder_ui.SessionHandler.CustomAuthorization;
 
 namespace finder_ui.Controllers
 {
-    public class MessageController : Controller
-    {//Will
-        //Test2
-        // GET: Message
+    // [Authorize]
+    [CustomAuthorization]
+    public class MessagesController : Controller
+    {
+        MessageServiceReference.Service1Client messageClient = new MessageServiceReference.Service1Client();
+        Group3ServiceReference.Service1Client adClient = new Group3ServiceReference.Service1Client();
         public ActionResult Index()
         {
+            ViewBag.sessId = Session["UserID"];
+            IEnumerable<MessageServiceReference.Messageinfo> messageList = messageClient.GetMessages().ToList();
+            ViewBag.Lista = messageList;
+
+            ViewBag.annonsClient = adClient.GetAllServiceData();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(MessageServiceReference.AddMessage newMessage)
+        {
+            MessageServiceReference.Service1Client messageClient = new MessageServiceReference.Service1Client();
+            messageClient.AddMessage(newMessage);
+            IEnumerable<MessageServiceReference.Messageinfo> messagelist = messageClient.GetMessages().ToList();
+            //IEnumerable<MeddelandeServiceReference.Messageinfo> messagelist = messageClient.GetMessages().ToList().Where(m => m.SendingUserId == 1);
+            ViewBag.Lista = messagelist;
             return View();
         }
     }
