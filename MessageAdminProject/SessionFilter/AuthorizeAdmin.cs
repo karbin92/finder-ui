ac0d94ae39ef53d99cedbed8dbd3f1769aead9d9
Pontushansson7 <47177677+Pontushansson7@users.noreply.github.com>
2019-05-23 09:29:44 +0200

    Admingränssnitt

---
+++ MessageAdminProject/SessionFilter/AuthorizeAdmin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MessageAdminProject.SessionFilter
+{
+    public class AuthorizeAdmin : System.Web.Mvc.ActionFilterAttribute, System.Web.Mvc.IActionFilter
+    {
+        public override void OnActionExecuting(System.Web.Mvc.ActionExecutingContext filterContext)
+        {
+            if(HttpContext.Current.Session["AdminIsLoggedIn"] == null)
+            {
+                filterContext.Result = new System.Web.Mvc.RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary
+                {
+                    {"Controller", "Home" },
+                    {"Action", "LogInAdmin" }
+                });
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
